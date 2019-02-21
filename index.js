8d9fb0cc6f69b7257778ec2295f45599e9b2e7b1
Michael McGovern <mmcgovern.wework@gmail.com>
2019-02-21 19:25:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 };
+happyHolidaysTo(you);
 
 function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
